886ffd2b4dc3a2c6671b85ed7643c0b6bb757c60
Derrick Marshall <marshallderrickdnm@gmail.com>
2019-12-01 18:56:27 -0800

    finshed server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3003;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use('/', express.static(path.join(__dirname,'../client/dist')))
 app.listen(port, () => console.log(`app listening on port ${port}!`));
