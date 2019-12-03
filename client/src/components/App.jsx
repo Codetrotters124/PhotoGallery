104b59cd0569bb916b182f9cb4a459cca13bd1a7
Derrick Marshall <marshallderrickdnm@gmail.com>
2019-12-03 14:22:31 -0800

    able to to see app.jsx file in html

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
+import { throws } from 'assert';
 const {ajax} = require('jquery');
 
 class App extends React.Component{
   constructor(props){
     super(props);
+    this.state = {gallery: []}
   }
 
   componentDidMount(){
     ajax({
       method: 'GET',
-      url: `/:${this.props.restName}/images`,
-      error: (err) => console.log('ERROR: method:GET url:'),
-      success: (data) => console.log('Success')
+      url: `/${this.props.restName}/images`,
+      error: (err) => console.log(`ERROR: method:GET url: /${this.props.restName}/images - ${err}`),
+      success: (data) => this.setState({gallery: data})
     });
   }
 
   render(){
     return(
       <div>
-        {console.log(this.props)}
+        {console.log(this.state.gallery)}
         <h1>Hello In App</h1>
       </div>);
   }
