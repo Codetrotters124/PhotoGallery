7933e82fccbcbb04659ef9413b55a33f99038928
Derrick Marshall <marshallderrickdnm@gmail.com>
2019-12-13 13:37:43 -0800

    deleted

--- client/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-var restName = 'absinthe-brasserie-and-bar';
-
-ReactDOM.render(<App restName={restName}/>, document.getElementById('app'));
